008ce9c9a909f00fffab31697ca5e9d8bd0d1393
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-01-22 12:05:43 +0300

    task002 made task Image gallery from topic Browser default actions

---
+++ tsk002 Browser default actions task Image gallery/js/script.js
@@ -0,0 +1,12 @@
+thumbs.addEventListener("click", (event) => {
+	// if element or his parent tag <a> target != null
+	let target = event.target.closest("a");
+	// cancel browser action if event.target include <a>
+	if (target && thumbs.contains(target)) {
+		event.preventDefault();
+        // replace src attribute in tag <img> on 'href' from event.target
+		largeImg.src = target.getAttribute("href");
+        // in attribute 'alt' write down in 'alt' largeImg
+       largeImg.alt = target.title
+	}
+});
